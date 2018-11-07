f148995a095c107a59118fc8a0aec82bb9d0ab75
Sabik <Sabik@DESKTOP-580EE3R>
2018-11-07 15:21:41 +0530

    Comments added in hello world program

--- JavaBasics/src/com/sab/java/basics/helloworld/HelloWorld.java
+++ JavaBasics/src/com/sab/java/basics/helloworld/HelloWorld.java
@@ -3,6 +3,7 @@ package com.sab.java.basics.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		//Print a text
 		System.out.println("Welcome to JavaBasics Programs Course");
 
 	}
